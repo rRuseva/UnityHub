cb381dd02c511e183b0a11bdac0cc18b467fd755
Radina <ruseva.radina@gmail.com>
2020-03-28 22:18:30 +0200

    Commit app

---
+++ Homework1_CameraFollow/Assets/Scripts/JumpingPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlayer : MonoBehaviour
+{
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public bool onGround = true;
+    
+    Rigidbody rb;
+    void Start() {
+        rb = GetComponent<Rigidbody>(); 
+        
+    }
+    
+    void Update() {
+
+        if (onGround) {
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                //if (rb.velocity.y < 0)
+                //{
+                //    //rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+                //    rb.velocity = Vector3.up * 10 * (fallMultiplier - 1) * Time.deltaTime;
+                //    Debug.Log("1");
+                //}
+                //else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+                //{
+                //   // rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+                //    rb.velocity = Vector3.up * 10 * (lowJumpMultiplier - 1) * Time.deltaTime;
+                //    Debug.Log("2");
+                //}
+
+                rb.velocity += new Vector3(0, 10, 0) * (lowJumpMultiplier - 1); // * Physics.gravity.y;
+                onGround = false;
+
+            }
+        }
+        
+    }
+    void OnCollisionEnter (Collision other) { 
+
+        if(other.gameObject.CompareTag("Ground")) {
+            onGround = true;
+        }
+    }
+}
---
+++ Homework1_CameraFollow/Assets/Scripts/MovingPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlayer : MonoBehaviour {
+    public float moveSpeed = 13;
+ 
+    void Update() {
+
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * Time.deltaTime * moveSpeed;
+
+        // moveDirection = new Vector3(Input.GetAxis("Horizontal"), heightMultiplier * Input.GetAxis("Jump"), Input.GetAxis("Vertical")).normalized * Time.deltaTime * moveSpeed;
+
+        Vector3 pointToLookAt = transform.position + moveDirection * 100;
+        //pointToLookAt.y = transform.position.y;
+
+        // NOT TESTED
+       // Vector3 smoothetMovement = Vector3.Lerp(transform.position, transform.position + moveDirection, 0.75f);
+        transform.position += moveDirection;
+        transform.LookAt(pointToLookAt);
+
+
+    }
+}
