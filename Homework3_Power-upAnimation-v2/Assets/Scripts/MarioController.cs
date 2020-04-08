8e75df8acc4b18fc2f3caa736971f588809b9c47
Radina <ruseva.radina@gmail.com>
2020-04-08 22:10:13 +0300

    Homework3 v2 (#1)
    * init new project with assets fromIvvan and Scripts from v1
    
    * objects added
    
    * Big Mario and Big MarioAnimations added
    
    * QuestionBlock animation added
    
    * questionBlock animation fixed, mushroom animation added
    
    * mushroom animationcontroller fixed
    
    * block objects in scene aligned
    
    * Mario animation rearanged to group small and big Mario's animations
    
    * mario animation controller fixed
    
    * some animation bugs fixed
    
    * animation'sfixed; trigger Mario Grow animation not finished
    
    * mushroom animation changed
    
    * Mushroom controller edited

---
+++ Homework3_Power-upAnimation-v2/Assets/Scripts/MarioController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioController : MonoBehaviour
+{
+    [SerializeField] private bool isSmall = true;
+    [SerializeField] private bool isGrowing = false;
+    [SerializeField] private bool lookingForMushroom = false;
+
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (!isSmall)
+        //    isGrowing = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.LogError("hit someting");
+        if (collision.gameObject.CompareTag("Mushroom") && !collision.gameObject.GetComponent<MushroomController>().isActivated) {
+            Debug.Log("hit mushroom");
+            //lookingForMushroom = true;
+            //animator.SetBool("LookingForMushroom", true);
+            isGrowing = false;
+            animator.SetBool("IsGrowing", false);
+        }
+        else if (collision.gameObject.CompareTag("Mushroom") && collision.gameObject.GetComponent<MushroomController>().isActivated) {
+            Debug.Log("hit looking mushroom");
+            //lookingForMushroom = false;
+            //animator.SetBool("LookingForMushroom", false);
+            isGrowing = true;
+            animator.SetBool("IsGrowing", true);
+            isSmall = false;
+            animator.SetBool("IsSmall", false);
+        }
+    }
+        
+}
---
+++ Homework3_Power-upAnimation-v2/Assets/Scripts/MushroomController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomController : MonoBehaviour
+{
+    //[SerializeField] 
+    public bool isActivated = false;
+    //[SerializeField] 
+    public bool isFalling = false;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+       // animator.enabled = true;
+        animator.SetBool("IsActivated", false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //if (isActivated) {
+        //    //animator.enabled = true;
+        //    isFalling = true;
+        //    animator.SetBool("IsFalling", true);
+        //}
+    }
+    void InitializeFall() {
+        if (isActivated) {
+            isFalling = true;
+            animator.SetBool("IsFalling", true);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //if (collision.gameObject.CompareTag("Player")) {
+        //    isActivated = true;
+        //    animator.SetBool("isActivated", true);
+        //}
+    }
+}
