70c7aaa9fbb8d621a33b9fbd0564e0cb67954486
Radina <ruseva.radina@gmail.com>
2020-04-12 15:44:56 +0300

    Mario animation changed - used two controllers instead of sub-state machines

--- Homework3_Power-upAnimation-v3/Assets/Scripts/MarioController.cs
+++ Homework3_Power-upAnimation-v3/Assets/Scripts/MarioController.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class MarioController : MonoBehaviour
 {
-    [SerializeField] private bool isSmall = true;
-    //[SerializeField] private bool isGrowing = false;
-    //[SerializeField] private bool lookingForMushroom = false;
-
+    public RuntimeAnimatorController marioBig;
 
     private Animator animator;
+
     // Start is called before the first frame update
     void Start() {
         animator = GetComponent<Animator>();
@@ -20,22 +18,9 @@ public class MarioController : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        Debug.LogError("hit someting");
-        if (collision.gameObject.CompareTag("Mushroom") && !collision.gameObject.GetComponent<MushroomController>().isActivated) {
-            Debug.Log("hit mushroom");
-            //lookingForMushroom = true;
-            //animator.SetBool("LookingForMushroom", true);
-            //isGrowing = false;
-            //animator.SetBool("IsGrowing", false);
-        }
-        else if (collision.gameObject.CompareTag("Mushroom") && collision.gameObject.GetComponent<MushroomController>().isFalling) {
-            Debug.Log("hit falling mushroom");
-            //lookingForMushroom = false;
-            //animator.SetBool("LookingForMushroom", false);
-            //isGrowing = true;
-            //animator.SetBool("IsGrowing", true);
-            isSmall = false;
-            animator.SetBool("IsSmall", false);
+
+        if (collision.gameObject.CompareTag("Mushroom") && collision.gameObject.GetComponent<MushroomController>().isFalling) {
+            animator.runtimeAnimatorController = marioBig;
         }
     }
         
