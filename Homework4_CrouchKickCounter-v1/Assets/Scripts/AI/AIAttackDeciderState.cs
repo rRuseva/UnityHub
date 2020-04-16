804d66ae1857034918dd325547975b6170ff82c4
Radina <ruseva.radina@gmail.com>
2020-04-17 00:29:37 +0300

    AI animations added; exit from crouch not fixed

--- Homework4_CrouchKickCounter-v1/Assets/Scripts/AI/AIAttackDeciderState.cs
+++ Homework4_CrouchKickCounter-v1/Assets/Scripts/AI/AIAttackDeciderState.cs
@@ -3,7 +3,8 @@
 public class AIAttackDeciderState : StateMachineBehaviour {
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		float rand = Random.value;
-		if      (rand <= 0.5f) { animator.SetTrigger("ShouldJump"); }
-		else if (rand <= 1.0f) { animator.SetTrigger("ShouldMoveToPlayer"); }
+		if      (rand <= 0.25f) { animator.SetTrigger("ShouldCrouch"); }
+		else if (rand <= 0.50f) { animator.SetTrigger("ShouldJump"); }
+		else if (rand <= 1.00f) { animator.SetTrigger("ShouldMoveToPlayer"); }
 	}
 }
