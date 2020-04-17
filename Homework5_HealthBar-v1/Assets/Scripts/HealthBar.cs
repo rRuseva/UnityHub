bdbb89123a8388dc604bfffaf90075846c94bad4
Radina <ruseva.radina@gmail.com>
2020-04-17 20:28:55 +0300

    health bar gradient removed

--- Homework5_HealthBar-v1/Assets/Scripts/HealthBar.cs
+++ Homework5_HealthBar-v1/Assets/Scripts/HealthBar.cs
@@ -5,14 +5,18 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider slider; 
+    public Slider slider;
+   // public Gradient gradient;
+  //  public Image fill; 
     public void SetHealth(int health) {
-        slider.value = health; 
+        slider.value = health;
+       // fill.color = gradient.Evaluate(slider.normalizedValue );
     }
 
     public void SetMaxHealth(int maxHealth) {
         slider.maxValue = maxHealth;
-        slider.value = maxHealth; 
+        slider.value = maxHealth;
+     //   gradient.Evaluate(1f);
     }
     // Start is called before the first frame update
     void Start()
