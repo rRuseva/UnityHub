493ec3242afe9e6597e97fee488df2fc1cd97392
Radina <ruseva.radina@gmail.com>
2020-04-17 16:41:16 +0300

    start homework 5 based on homework4

---
+++ Nomework5_HealthBar-v1/Assets/Scripts/Player/MonkCrouchState.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using static Controlls;
+using static StateMachineUtil;
+
+public class MonkCrouchState : StateMachineBehaviour {
+    
+    private MovementController movementController;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+       // isCrouching = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        if (Input.GetKeyDown(attackKey)) {
+            animator.SetBool("IsCrouchKicking", true);
+        }
+        else if (Input.GetKeyUp(crouchKey)) { 
+            animator.SetBool("IsCrouching", false); 
+        }
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
