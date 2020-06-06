687a4b07ebf1ef4418506d17a2c0c8a2b25ab8e6
Radina <ruseva.radina@gmail.com>
2020-06-06 23:47:22 +0300

    Jump functuallity added; jump animation added;

--- EndlessRunner_CourseProject/Assets/Scripts/CameraFollower.cs
+++ EndlessRunner_CourseProject/Assets/Scripts/CameraFollower.cs
@@ -3,14 +3,17 @@
 public class CameraFollower : MonoBehaviour {
     private Transform playerTransform;
     private float offset; // the offset in the scene before 'Play'
+    private float offsetY;
 
     private void Start() {
         playerTransform = GameObject.FindWithTag("Player").transform;
-        offset = playerTransform.position.x - transform.position.x;
+        offset = playerTransform.position.x - transform.position.x ;
+
+        offsetY = transform.position.y - playerTransform.position.y ;
     }
 
     private void Update() {
-        Vector3 newCameraPosition = new Vector3(playerTransform.position.x - offset, transform.position.y, playerTransform.position.z);
+        Vector3 newCameraPosition = new Vector3(playerTransform.position.x - offset, playerTransform.position.y + offsetY, playerTransform.position.z);
         transform.position = Vector3.Lerp(transform.position, newCameraPosition, 0.1f);
     }
 }
--- EndlessRunner_CourseProject/Assets/Scripts/PlayerMovementController.cs
+++ EndlessRunner_CourseProject/Assets/Scripts/PlayerMovementController.cs
@@ -5,19 +5,33 @@ public class PlayerMovementController : MonoBehaviour {
     private readonly float OFFSET_FROM_EDGES = 1.8f;
     private readonly float SINGLE_HORIZONTAL_MOVEMENT_DISTANCE = 70f;
     private float horizontalDistanceToMove;
+    //vertical movement: 
+    private bool isGrounded = true;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 0.8f;
+    [SerializeField] private float jumpVelocity = 7.0f;
 
+    Animator animator; 
     private void Start() {
         InputRecognizer.swipe += OnMove;
         horizontalDistanceToMove = 0;
+        animator = GetComponent<Animator>();
+
     }
 
     private void Update() {
         Vector3 moveForwardDirection = Time.deltaTime * new Vector3(runningForwardSpeed, 0, 0);
         Vector3 moveHorizontallyDirection = Time.deltaTime * new Vector3(0, 0, horizontalDistanceToMove);
         horizontalDistanceToMove = 0;
+        if (isGrounded) 
+            verticalVelocity = 0.0f;
+        else 
+            Fall();
 
-        Vector3 finalPosition = transform.position + moveForwardDirection + moveHorizontallyDirection;
+        Vector3 moveVerticalDirection = Time.deltaTime * new Vector3(0, verticalVelocity, 0);
+        Vector3 finalPosition = transform.position + moveForwardDirection + moveHorizontallyDirection + moveVerticalDirection;
         transform.position = ValidatePositionIfOutsidePath(finalPosition);
+        
     }
 
     private void OnDisable() {
@@ -27,10 +41,14 @@ public class PlayerMovementController : MonoBehaviour {
     void OnMove(SwipeDirection direction) {
         if (IsSwipeDirectionHorizontal(direction)) {
             horizontalDistanceToMove = calculateDistanceToMove(direction);
-        } else if (direction == SwipeDirection.Up) {
+        }
+        else if (direction == SwipeDirection.Up) {
             // TODO: Start jump animation
-        } else if (direction == SwipeDirection.Down) {
-            // TODO: Start chouch animation
+            Jump();
+            
+        }
+        else if (direction == SwipeDirection.Down) {
+            // TODO: Start crouch animation
         }
     }
 
@@ -47,5 +65,41 @@ public class PlayerMovementController : MonoBehaviour {
     private Vector3 ValidatePositionIfOutsidePath(Vector3 positionToValidate) {
         float validHorizontalPosition = Mathf.Clamp(positionToValidate.z, -OFFSET_FROM_EDGES, OFFSET_FROM_EDGES);
         return new Vector3(positionToValidate.x, positionToValidate.y, validHorizontalPosition);
+    }
+
+    public void Jump() {
+        if (isGrounded) {
+            animator.SetTrigger("Jump");
+            verticalVelocity += jumpVelocity;
+            isGrounded = false;
+
+        }
+    }
+    public void Fall() {
+        verticalVelocity -= gravity * Time.deltaTime;
+    }
+    private bool isOnGround() {
+        int mask = LayerMask.GetMask("Default");
+        float rayLength = 0.99f;
+        Vector3 origin = transform.position + new Vector3(0, 0.5f, 0);
+
+        Vector3 rayDirection = new Vector3(0, -1, 0);
+
+        if (Physics.Raycast(origin, rayDirection,
+                                out RaycastHit hit, rayLength, mask, QueryTriggerInteraction.Collide)) {
+            if (hit.collider.tag.Equals("Ground")) {
+                isGrounded = true;
+            }
+
+        }
+        isGrounded = false;
+        return isGrounded;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+            verticalVelocity = 0;
+        }
     }
 }
