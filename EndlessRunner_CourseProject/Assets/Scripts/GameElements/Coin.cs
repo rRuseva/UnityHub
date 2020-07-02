831ff5a7ba1fd6cccfcace190b7f8f7d464ff87b
Radina <ruseva.radina@gmail.com>
2020-07-02 22:37:17 +0300

    added rotation to coins; fixed colliders

--- EndlessRunner_CourseProject/Assets/Scripts/GameElements/Coin.cs
+++ EndlessRunner_CourseProject/Assets/Scripts/GameElements/Coin.cs
@@ -1,12 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class Coin : MonoBehaviour, IPooledObject {
+
+    public float RotationSpeed = 20.0f;
     public void OnObjectSpawn() {
-        Quaternion rotation = Quaternion.Euler(1, 5, 1);
-        transform.rotation = rotation;
+        if (UnityEngine.Random.value >= 0.5) {
+            Vector3 rot = this.transform.rotation.eulerAngles;
+            rot = new Vector3(rot.x, rot.y + 45, rot.z);
+            this.transform.rotation = Quaternion.Euler(rot);
+        }
     }
 
     private void Update() {
-        transform.Rotate(Vector3.up * (100 * Time.deltaTime));
+        transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
     }
 }
--- EndlessRunner_CourseProject/Assets/Scripts/GameElements/slantedTree.cs
+++ EndlessRunner_CourseProject/Assets/Scripts/GameElements/slantedTree.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree : MonoBehaviour, IPooledObject {
+public class slantedTree : MonoBehaviour, IPooledObject {
     public void OnObjectSpawn() {
-        if (Random.value >= 0.5) {
+       if( Random.value >= 0.7){
             Vector3 rot = this.transform.rotation.eulerAngles;
-            rot = new Vector3(rot.x, rot.y + 90, rot.z);
+            rot = new Vector3(rot.x, rot.y + 180, rot.z +14);
             this.transform.rotation = Quaternion.Euler(rot);
         }
     }
-
 }
